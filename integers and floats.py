8379994f5916a3bb609d30e60243a75b2fccee66
Ayiekoo <131998667+Ayiekoo@users.noreply.github.com>
2023-06-15 17:26:41 +0300

    Add files via upload

---
+++ integers and floats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+### intergers and floats
+
+#### arithmetic operations
+
+x, y = 5, 9
+print(x + y) ## addition of integers
+print(x - y) ## subtraction of integers
+print(x * y) ## multiplication
+print(x / y) # division
+print(x // y) ##  divides the first operand by the second operand and then rounds the result down to the nearest integer. 
+print(x % y) ## computes the reminder of the devision of the two numbers
+print(-x) ## prints the negative value of the number
+print(abs(-x)) ## gives the absolute value of the value
+print(int(9.9)) # truncates the decimal, converting the float to an integer
+print(float(5)) # adds a decimal to the float, making it an integer
+print(x ** y)
+
+
+# In[2]:
+
+
+print(10 % 7) ## computes the reminder of the devision of the two numbers
+
+
+# In[ ]:
+
+
+
+
